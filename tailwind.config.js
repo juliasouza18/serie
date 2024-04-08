0ef555c78188efb82ff3044ec590d073e41df5c6
EA - Julia Souza Costa <julia_costa01@educadventista.org>
2024-04-08 10:03:03 -0300

    comeco da estilização

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from "tailwindcss/colors"
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors:{
+      'pink': '#f472b6',
+      'branco': '#fff',
+      'red': '#dc2626',
+      'pink2': '#9d174d',
+      'pink3': '#fbcfe8',
+      'roxo': '#86198f',
+      pink: colors.pink,
+    }
   },
   plugins: [],
 }
-
