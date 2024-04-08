96a3c4a0e999e69a33b7233c634014a740802bac
EA - Julia Souza Costa <julia_costa01@educadventista.org>
2024-04-08 09:23:56 -0300

    informações gerais da serie

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Chicago from './components/Chicago'
+
+export default function App() {
+  return (
+    <div>
+      <Chicago/>
+    </div>
+  )
+}
---
+++ src/components/Chicago.jsx
@@ -0,0 +1,36 @@
+import chicago1 from "../assets/chicago1.jpg"
+
+
+export default function Chicago() {
+  return (
+    <section>
+        <h2 className="font-medium text-4xl">Chicago P.D</h2>
+        <img src={chicago1} alt="Capa da série Chicago P.D" />
+        <p>O spin-off de Chicago Fire, Chicago P.D. se concentra no fictício 21º Distrito, que abriga patrulheiros e a unidade de inteligência de elite do departamento, liderada pelo detetive-sargento Hank Voight (Jason Beghe). As três primeiras temporadas e a primeira metade da quarta temporada se concentram na patrulha e nos oficiais de inteligência, mas o programa muda para se concentrar na Unidade de Inteligência a partir da segunda metade da quarta temporada, depois que o oficial Kevin Atwater (LaRoyce Hawkins) e a oficial Kim Burgess (Marina Squerciati) avançam para a Unidade de Inteligência.</p>
+        <p>Autores
+Matt Olmstead Roteirista
+Michael Brandt Roteirista
+Dick Wolf Roteirista
+Tim Walsh Roteirista
+Derek Haas Roteirista
+Produtores
+Danielle Gelber Produtora Executiva
+Arthur W. Forney Coprodutor executivo
+Peter Jankowski Produtor Executivo
+Mark Tinker Produtor Executivo
+Direção
+Mark Tinker Diretor
+Nick Gomez Diretor
+Sanford Bookstaver Diretor
+Michael Slovis Diretor
+Karen Gaviola Diretor
+Alik Sakharov Diretor
+Fred Berner Diretor
+Joe Chappelle Diretor
+Empresas envolvidas
+Wolf Films Produção
+Universal Television Produção</p>
+    </section>
+    
+  )
+}
